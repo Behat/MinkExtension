3ee16c4b871e5c39140f4c02db912507bed5c39f
Christophe Coevoet <stof@notk.org>
2014-01-11 00:04:23 +0100

    Refactored the configuration of sessions
    The configuration now makes sessions a first-class citizen instead of
    being centered around drivers. This allows defining several session based
    on the same driver type.
    Instead of allowing other extensions to register their own sessions to add
    new drivers, they can now register a DriverFactory in the MinkExtension
    during the extension initialization to make a new driver type available.

---
+++ spec/Behat/MinkExtension/ServiceContainer/Driver/SeleniumFactorySpec.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace spec\Behat\MinkExtension\ServiceContainer\Driver;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class SeleniumFactorySpec extends ObjectBehavior
+{
+    function it_is_a_driver_factory()
+    {
+        $this->shouldHaveType('Behat\MinkExtension\ServiceContainer\Driver\DriverFactory');
+    }
+
+    function it_is_named_selenium()
+    {
+        $this->getDriverName()->shouldReturn('selenium');
+    }
+
+    function it_supports_javascript()
+    {
+        $this->supportsJavascript()->shouldBe(true);
+    }
+}
---
+++ spec/Behat/MinkExtension/ServiceContainer/Driver/ZombieFactorySpec.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace spec\Behat\MinkExtension\ServiceContainer\Driver;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ZombieFactorySpec extends ObjectBehavior
+{
+    function it_is_a_driver_factory()
+    {
+        $this->shouldHaveType('Behat\MinkExtension\ServiceContainer\Driver\DriverFactory');
+    }
+
+    function it_is_named_zombie()
+    {
+        $this->getDriverName()->shouldReturn('zombie');
+    }
+
+    function it_supports_javascript()
+    {
+        $this->supportsJavascript()->shouldBe(true);
+    }
+}
